9f65694ee8bcd1074d7998b820ec81dd097fe01c
Adrian <adrian.ortega.eerr@gmail.com>
2022-02-07 23:19:29 +0100

    Delete deprecated logic

--- src/services/getImages.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-export const getImages = async (couplesCount) => {
-  const response = await axios.get(
-    `https://picsum.photos/v2/list?limit=${couplesCount}`
-  );
-  const images = response.data.map((img) => img.download_url);
-  return images;
-};
