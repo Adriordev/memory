bc118951751fb2a7d5cb23d18b4bd62b02188a3b
Adrian <adrian.ortega.eerr@gmail.com>
2022-02-08 21:48:38 +0100

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      keyframes: {
+        'fadeInDown': {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-50px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        'fadeInDown': "fade-in-down 1s ease-out",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
